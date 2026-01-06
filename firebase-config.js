09fdb10444e2a01e409dd710516d30fffd349b83
loading1234t <ddimonov461@gmail.com>
2026-01-07 04:53:52 +0500

    Update and rename irebase-config.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDcyOWAF7TA8h8P36Sk5EbDPzA9sEFuTXo",
+    authDomain: "ggpoint-shop.firebaseapp.com",
+    databaseURL: "https://ggpoint-shop-default-rtdb.firebaseio.com",
+    projectId: "ggpoint-shop",
+    storageBucket: "ggpoint-shop.firebasestorage.app",
+    messagingSenderId: "182227173652",
+    appId: "1:182227173652:web:0949e2edae9900afd98201",
+    measurementId: "G-V76H7K3VR4"
+};
